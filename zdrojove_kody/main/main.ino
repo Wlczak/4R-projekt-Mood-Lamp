16bdbab8ccfee591c43669c81ae2c2d9d69916b4
Adam Vlček <vlcek-adam@seznam.cz>
2025-02-26 01:20:37 +0100

    added frame counter && OTA

--- zdrojove_kody/main/main.ino
+++ zdrojove_kody/main/main.ino
@@ -1,10 +1,26 @@
 #include <WiCo.h>
 WiCo wico;
+int frames = 0;
+long milli = millis();
+bool countFrames = true;
 void setup() {
+  Serial.begin(115200);
   wico.addSTA("wemos", "wemosD1R2");
   wico.connectSTA();
+  wico.setOTAsettings("mood_lamp", "8266", 8266);
+  wico.startOTA();
 }
-
+void calcFramerate() {
+  frames++;
+  if (millis() - milli > 1000) {
+    Serial.println(frames);
+    milli = millis();
+    frames = 0;
+  }
+}
 void loop() {
-  // put your main code here, to run repeatedly:
+  wico.handleOTA();
+  if (countFrames) {
+    calcFramerate();
+  }
 }
