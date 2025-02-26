fbf4fe98be57377a1051d8f68068fc1bf2a48c46
Adam Vlček <vlcek-adam@seznam.cz>
2025-02-26 13:56:48 +0100

    MQTT implementation

--- zdrojove_kody/main/main.ino
+++ zdrojove_kody/main/main.ino
@@ -2,13 +2,18 @@
 WiCo wico;
 int frames = 0;
 long milli = millis();
-bool countFrames = true;
+bool countFrames = false;
 void setup() {
   Serial.begin(115200);
   wico.addSTA("wemos", "wemosD1R2");
   wico.connectSTA();
   wico.setOTAsettings("mood_lamp", "8266", 8266);
   wico.startOTA();
+  wico.setMQTTAuth("wemos", "wemosR1D2");
+  wico.setMQTTId("mood_lamp");
+  wico.connectMQTT("192.168.0.106", 1883);
+  wico.publishMQTT("test", "hello there :)");
+  wico.subscribeMQTT("test", echo);
 }
 void calcFramerate() {
   frames++;
@@ -18,8 +23,18 @@ void calcFramerate() {
     frames = 0;
   }
 }
+
+void echo(const char* topic, const char* msg) {
+  Serial.println("This is the echo function.");
+  Serial.print("Topic: ");
+  Serial.println(topic);
+  Serial.print("Msg: ");
+  Serial.println(msg);
+}
+
 void loop() {
-  wico.handleOTA();
+  wico.runOTA();
+  wico.runMQTT();
   if (countFrames) {
     calcFramerate();
   }
