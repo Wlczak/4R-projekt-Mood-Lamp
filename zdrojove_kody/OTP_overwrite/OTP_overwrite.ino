a2ca61700c3957a37d4dab6407e3bf8f351bfa36
Adam Vlček <vlcek-adam@seznam.cz>
2025-03-24 17:43:41 +0100

    Update OTP_overwrite.ino

--- zdrojove_kody/OTP_overwrite/OTP_overwrite.ino
+++ zdrojove_kody/OTP_overwrite/OTP_overwrite.ino
@@ -2,9 +2,11 @@ const int led = 2;
 
 void setup() {
   pinMode(led, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
+  Serial.println("hi");
   digitalWrite(led, HIGH);
   delay(1000);
   digitalWrite(led, LOW);
