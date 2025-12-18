db1ae8f4984a17bc2c361d9affc6d2495deea666
Adam Vlček <vlcek-adam@seznam.cz>
2025-12-18 19:52:53 +0100

    updated rgb rendering

--- zdrojove_kody/main_rework/main_rework.ino
+++ zdrojove_kody/main_rework/main_rework.ino
@@ -17,6 +17,67 @@ int temperature = 0;
 int humidity = 0;
 long dht_millis = 0;
 
+int curR = 0;
+int curG = 0;
+int curB = 0;
+int curBrightness = 0;
+
+int targetR = 0;
+int targetG = 0;
+int targetB = 0;
+int targetBrightness = 0;
+
+unsigned long lastFade = 0;
+const unsigned long fadeInterval = 20;
+const int fadeStep = 1;
+
+void applyPixels() {
+  for (int i = 0; i < PIXELS; i++) {
+    neofruit.setPixelColor(i, neofruit.Color(curR, curG, curB));
+  }
+  neofruit.setBrightness(curBrightness);
+  neofruit.show();
+}
+
+int stepTowards(int current, int target) {
+  if (current < target) return min(current + fadeStep, target);
+  if (current > target) return max(current - fadeStep, target);
+  return current;
+}
+
+void rgb(const char *topic, const char *msg) {
+  if (strlen(msg) != 6) return;
+
+  targetR = strtol(String(msg).substring(0, 2).c_str(), nullptr, 16);
+  targetG = strtol(String(msg).substring(2, 4).c_str(), nullptr, 16);
+  targetB = strtol(String(msg).substring(4, 6).c_str(), nullptr, 16);
+
+  Serial.print("New RGB target: ");
+  Serial.println(msg);
+}
+
+void brightness(const char *topic, const char *msg) {
+  int b = atoi(msg);
+  b = constrain(b, 0, 100);
+  targetBrightness = map(b, 0, 100, 0, 255);
+
+  Serial.print("New brightness target: ");
+  Serial.println(targetBrightness);
+}
+
+void updateFade() {
+  if (millis() - lastFade < fadeInterval) return;
+  lastFade = millis();
+
+  curR = stepTowards(curR, targetR);
+  curG = stepTowards(curG, targetG);
+  curB = stepTowards(curB, targetB);
+  curBrightness = stepTowards(curBrightness, targetBrightness);
+
+  applyPixels();
+}
+
+
 void setColor(int r, int g, int b) {
   delay(600);
   for (int i = 0; i < PIXELS; i++) {
@@ -64,11 +125,12 @@ void setup() {
   Serial.println(WiFi.SSID());
   setupMQTT();
   setColor(0, 0, 0);
-  wico.subscribeMQTT("mood_lamp/rgb", mqttPrint);
-  wico.subscribeMQTT("mood_lamp/brightness", mqttPrint);
+  wico.subscribeMQTT("mood_lamp/rgb", rgb);
+  wico.subscribeMQTT("mood_lamp/brightness", brightness);
   neofruit.setBrightness(0);
 }
 
 void loop() {
   wico.runMQTT();
+  updateFade();
 }
